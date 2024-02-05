5d1f53231618378db174511df757bcac5fba92f9
Jorge Barrios <44713671+jBarrios26@users.noreply.github.com>
2024-02-04 22:46:15 -0600

    feat: add to string to cube

--- lib/3x3/cube_3x3.dart
+++ lib/3x3/cube_3x3.dart
@@ -1,13 +1,23 @@
+import 'package:puzzle_cube/3x3/three_by_three.dart';
 import 'package:puzzle_cube/model/model.dart';
 
 class Cube3x3 extends Cube {
   int movesPerformed = 0;
+  Map<Face, CubeColor> colorScheme;
 
-  Cube3x3({
-    required super.centers,
-    required super.corners,
-    required super.edge,
-  });
+  Cube3x3(
+      {required super.centers,
+      required super.corners,
+      required super.edge,
+      this.colorScheme = classicColorScheme});
+
+  factory Cube3x3.solved({final Map<Face, CubeColor>? color}) {
+    return Cube3x3(
+      centers: Centers3x3.solved(colors: color ?? classicColorScheme),
+      corners: Corners3x3.solved(colors: color ?? classicColorScheme),
+      edge: Edges3x3.solved(colors: color ?? classicColorScheme),
+    );
+  }
 
   @override
   void b() {
@@ -149,5 +159,277 @@ class Cube3x3 extends Cube {
     edge.uAntiClockwise();
     centers.uAntiClockwise();
     movesPerformed++;
+  }
+
+  @override
+  String toString() {
+    //UFL
+    final u7 = corners.corners[CornerPosition.ufl]!.getColorBasedOnPosition(
+      CornerPosition.ufl,
+      Face.up,
+    );
+    final f1 = corners.corners[CornerPosition.ufl]!.getColorBasedOnPosition(
+      CornerPosition.ufl,
+      Face.front,
+    );
+    final l3 = corners.corners[CornerPosition.ufl]!.getColorBasedOnPosition(
+      CornerPosition.ufl,
+      Face.left,
+    );
+
+    //URF
+    final u9 = corners.corners[CornerPosition.urf]!.getColorBasedOnPosition(
+      CornerPosition.urf,
+      Face.up,
+    );
+    final r1 = corners.corners[CornerPosition.urf]!.getColorBasedOnPosition(
+      CornerPosition.urf,
+      Face.right,
+    );
+    final f3 = corners.corners[CornerPosition.urf]!.getColorBasedOnPosition(
+      CornerPosition.urf,
+      Face.front,
+    );
+    //UBR
+    final u3 = corners.corners[CornerPosition.ubr]!.getColorBasedOnPosition(
+      CornerPosition.ubr,
+      Face.up,
+    );
+    final b1 = corners.corners[CornerPosition.ubr]!.getColorBasedOnPosition(
+      CornerPosition.ubr,
+      Face.back,
+    );
+    final r3 = corners.corners[CornerPosition.ubr]!.getColorBasedOnPosition(
+      CornerPosition.ubr,
+      Face.right,
+    );
+
+    //ULB
+    final u1 = corners.corners[CornerPosition.ulb]!.getColorBasedOnPosition(
+      CornerPosition.ulb,
+      Face.up,
+    );
+    final l1 = corners.corners[CornerPosition.ulb]!.getColorBasedOnPosition(
+      CornerPosition.ulb,
+      Face.left,
+    );
+    final b3 = corners.corners[CornerPosition.ulb]!.getColorBasedOnPosition(
+      CornerPosition.ulb,
+      Face.back,
+    );
+
+    //DLF
+    final d1 = corners.corners[CornerPosition.dlf]!.getColorBasedOnPosition(
+      CornerPosition.dlf,
+      Face.down,
+    );
+    final l9 = corners.corners[CornerPosition.dlf]!.getColorBasedOnPosition(
+      CornerPosition.dlf,
+      Face.left,
+    );
+    final f7 = corners.corners[CornerPosition.dlf]!.getColorBasedOnPosition(
+      CornerPosition.dlf,
+      Face.front,
+    );
+
+    //DFR
+    final d3 = corners.corners[CornerPosition.dfr]!.getColorBasedOnPosition(
+      CornerPosition.dfr,
+      Face.down,
+    );
+    final f9 = corners.corners[CornerPosition.dfr]!.getColorBasedOnPosition(
+      CornerPosition.dfr,
+      Face.front,
+    );
+    final r7 = corners.corners[CornerPosition.dfr]!.getColorBasedOnPosition(
+      CornerPosition.dfr,
+      Face.right,
+    );
+
+    //DRB
+    final d9 = corners.corners[CornerPosition.drb]!.getColorBasedOnPosition(
+      CornerPosition.drb,
+      Face.down,
+    );
+    final r9 = corners.corners[CornerPosition.drb]!.getColorBasedOnPosition(
+      CornerPosition.drb,
+      Face.right,
+    );
+    final b7 = corners.corners[CornerPosition.drb]!.getColorBasedOnPosition(
+      CornerPosition.drb,
+      Face.back,
+    );
+
+    //DBL
+    final d7 = corners.corners[CornerPosition.dbl]!.getColorBasedOnPosition(
+      CornerPosition.dbl,
+      Face.down,
+    );
+    final l7 = corners.corners[CornerPosition.dbl]!.getColorBasedOnPosition(
+      CornerPosition.dbl,
+      Face.left,
+    );
+    final b9 = corners.corners[CornerPosition.dbl]!.getColorBasedOnPosition(
+      CornerPosition.dbl,
+      Face.back,
+    );
+
+    // UF
+    final u8 = edge.edges[(EdgePosition.uf, edgeFirstLevel)]!
+        .getFaceletBasedByPosition(
+      EdgePosition.uf,
+      Face.up,
+    );
+    final f2 = edge.edges[(EdgePosition.uf, edgeFirstLevel)]!
+        .getFaceletBasedByPosition(
+      EdgePosition.uf,
+      Face.front,
+    );
+
+    // UR
+    final u6 = edge.edges[(EdgePosition.ur, edgeFirstLevel)]!
+        .getFaceletBasedByPosition(
+      EdgePosition.ur,
+      Face.up,
+    );
+    final r2 = edge.edges[(EdgePosition.ur, edgeFirstLevel)]!
+        .getFaceletBasedByPosition(
+      EdgePosition.ur,
+      Face.right,
+    );
+
+    // UB
+    final u2 = edge.edges[(EdgePosition.ub, edgeFirstLevel)]!
+        .getFaceletBasedByPosition(
+      EdgePosition.ub,
+      Face.up,
+    );
+    final b2 = edge.edges[(EdgePosition.ub, edgeFirstLevel)]!
+        .getFaceletBasedByPosition(
+      EdgePosition.ub,
+      Face.back,
+    );
+
+    // UL
+    final u4 = edge.edges[(EdgePosition.ul, edgeFirstLevel)]!
+        .getFaceletBasedByPosition(
+      EdgePosition.ul,
+      Face.up,
+    );
+    final l2 = edge.edges[(EdgePosition.ul, edgeFirstLevel)]!
+        .getFaceletBasedByPosition(
+      EdgePosition.ul,
+      Face.left,
+    );
+
+    // FL
+    final f4 = edge.edges[(EdgePosition.fl, edgeFirstLevel)]!
+        .getFaceletBasedByPosition(
+      EdgePosition.fl,
+      Face.front,
+    );
+    final l6 = edge.edges[(EdgePosition.fl, edgeFirstLevel)]!
+        .getFaceletBasedByPosition(
+      EdgePosition.fl,
+      Face.left,
+    );
+
+    // FR
+    final f6 = edge.edges[(EdgePosition.fr, edgeFirstLevel)]!
+        .getFaceletBasedByPosition(
+      EdgePosition.fr,
+      Face.front,
+    );
+    final r4 = edge.edges[(EdgePosition.fr, edgeFirstLevel)]!
+        .getFaceletBasedByPosition(
+      EdgePosition.fr,
+      Face.right,
+    );
+
+    // BR
+    final b4 = edge.edges[(EdgePosition.br, edgeFirstLevel)]!
+        .getFaceletBasedByPosition(
+      EdgePosition.br,
+      Face.back,
+    );
+    final r6 = edge.edges[(EdgePosition.br, edgeFirstLevel)]!
+        .getFaceletBasedByPosition(
+      EdgePosition.br,
+      Face.right,
+    );
+
+    // BL
+    final b6 = edge.edges[(EdgePosition.bl, edgeFirstLevel)]!
+        .getFaceletBasedByPosition(
+      EdgePosition.bl,
+      Face.back,
+    );
+    final l4 = edge.edges[(EdgePosition.bl, edgeFirstLevel)]!
+        .getFaceletBasedByPosition(
+      EdgePosition.bl,
+      Face.left,
+    );
+
+    // DF
+    final d2 = edge.edges[(EdgePosition.df, edgeFirstLevel)]!
+        .getFaceletBasedByPosition(
+      EdgePosition.df,
+      Face.down,
+    );
+    final f8 = edge.edges[(EdgePosition.df, edgeFirstLevel)]!
+        .getFaceletBasedByPosition(
+      EdgePosition.df,
+      Face.front,
+    );
+
+    // DR
+    final d6 = edge.edges[(EdgePosition.dr, edgeFirstLevel)]!
+        .getFaceletBasedByPosition(
+      EdgePosition.dr,
+      Face.down,
+    );
+    final r8 = edge.edges[(EdgePosition.dr, edgeFirstLevel)]!
+        .getFaceletBasedByPosition(
+      EdgePosition.dr,
+      Face.right,
+    );
+
+    // DB
+    final d8 = edge.edges[(EdgePosition.db, edgeFirstLevel)]!
+        .getFaceletBasedByPosition(
+      EdgePosition.db,
+      Face.down,
+    );
+    final b8 = edge.edges[(EdgePosition.db, edgeFirstLevel)]!
+        .getFaceletBasedByPosition(
+      EdgePosition.db,
+      Face.back,
+    );
+
+    // DL
+    final d4 = edge.edges[(EdgePosition.dl, edgeFirstLevel)]!
+        .getFaceletBasedByPosition(
+      EdgePosition.dl,
+      Face.down,
+    );
+    final l8 = edge.edges[(EdgePosition.dl, edgeFirstLevel)]!
+        .getFaceletBasedByPosition(
+      EdgePosition.dl,
+      Face.left,
+    );
+
+    final cubeTemplete = '''
+               |$u1 $u2 $u3|
+               |$u4 u5 $u6|
+               |$u7 $u8 $u9|
+      |$l1 $l2 $l3|$f1 $f2 $f3|$r1 $r2 $r3|$b1 $b2 $b3|
+      |$l4 l5 $l6|$f4 f5 $f6|$r4 r5 $r6|$b4 b5 $b6|
+      |$l7 $l8 $l9|$f7 $f8 $f9|$r7 $r8 $r9|$b7 $b8 $b9|
+               |$d1 $d2 $d3|
+               |$d4 d5 $d6|
+               |$d7 $d8 $d9|
+    ''';
+
+    return cubeTemplete;
   }
 }
