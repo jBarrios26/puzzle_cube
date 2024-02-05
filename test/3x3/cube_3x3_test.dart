2544797b8384b1a1d4c25cdce0d13bb7b8f7c91d
Jorge Barrios <44713671+jBarrios26@users.noreply.github.com>
2024-02-04 22:46:47 -0600

    feat: add test to check toString in 3x3 cube.

---
+++ test/3x3/cube_3x3_test.dart
@@ -0,0 +1,29 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:puzzle_cube/3x3/three_by_three.dart';
+
+void main() {
+  late Cube3x3 cube;
+  setUp(() {
+    cube = Cube3x3.solved();
+  });
+
+  test(
+    'solved cube shoud return correct toString template',
+    () {
+      const cubeToString =
+          '''
+               |u1 u2 u3|
+               |u4 u5 u6|
+               |u7 u8 u9|
+      |l1 l2 l3|f1 f2 f3|r1 r2 r3|b1 b2 b3|
+      |l4 l5 l6|f4 f5 f6|r4 r5 r6|b4 b5 b6|
+      |l7 l8 l9|f7 f8 f9|r7 r8 r9|b7 b8 b9|
+               |d1 d2 d3|
+               |d4 d5 d6|
+               |d7 d8 d9|
+    ''';
+      final result = cube.toString();
+      expect(result, cubeToString);
+    },
+  );
+}
